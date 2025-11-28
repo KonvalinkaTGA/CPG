c6a5adff0ed35a41d431dd21f48dda78ffdf06c9
KonvalinkaTGA <throw_away@centrum.cz>
2025-11-28 23:40:26 +0100

    Add files via upload

---
+++ fifth.py
@@ -0,0 +1,91 @@
+
+import sys
+
+# definice úvodních binárních sekvencí obrázkových souborů
+jpeg_header = b'\xff\xd8'
+gif_header1 = b'GIF87a'
+gif_header2 = b'GIF89a'
+png_header = b'\x89PNG\r\n\x1a\n'
+
+
+def read_header(file_name, header_length):
+    """
+    Tato funkce načte binární soubor z cesty file_name,
+    z něj přečte prvních header_length bytů a ty vrátí pomocí return
+    """
+    with open(file_name, "rb") as f:
+        return f.read(header_length)
+
+
+def is_jpeg(file_name):
+    """
+    Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
+    tu srovná s definovanou hlavičkou v proměnné jpeg_header
+    """
+    # načti hlavičku souboru
+    header = read_header(file_name, len(jpeg_header))
+    return header == jpeg_header
+
+    # vyhodnoť zda je soubor jpeg
+    
+    return False
+
+
+def is_gif1(file_name):
+    """
+    Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
+    tu srovná s definovanými hlavičkami v proměnných gif_header1 a gif_header2
+    """
+    header = read_header(file_name, len(gif_header1))
+    return header == gif_header1
+    # vyhodnoť zda je soubor gif
+    
+
+def is_gif2(file_name):
+    """
+    Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
+    tu srovná s definovanými hlavičkami v proměnných gif_header1 a gif_header2
+    """
+    header = read_header(file_name, len(gif_header2))
+    return header == gif_header2
+    # vyhodnoť zda je soubor gif
+    
+
+
+def is_png(file_name):
+    """
+    Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
+    tu srovná s definovanou hlavičkou v proměnné png_header
+    """
+    # vyhodnoť zda je soubor png
+    header = read_header(file_name, len(png_header))
+    return header == png_header
+
+
+def print_file_type(file_name):
+    """
+    Funkce vypíše typ souboru - tuto funkci není třeba upravovat
+    """
+    if is_jpeg(file_name):
+        print(f'Soubor {file_name} je typu jpeg')
+    elif is_gif1(file_name) or is_gif2(file_name):
+        print(f'Soubor {file_name} je typu gif')
+    elif is_png(file_name):
+        print(f'Soubor {file_name} je typu png')
+    else:
+        print(f'Soubor {file_name} je neznámého typu')
+
+
+if __name__ == '__main__':
+    try:
+        if len(sys.argv) > 1:
+            file_name = sys.argv[1]
+        else:
+            file_name = input("Zadej cestu k souboru: ")
+            # možnost zadat cestu k souboru ručně
+
+        print_file_type(file_name)
+
+    except Exception as e:
+        print("Nastala chyba:", e)
+    
